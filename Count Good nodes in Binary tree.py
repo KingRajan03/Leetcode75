4645bb6f001709654e64abf3ecdee46e6df83ae9
KingRajan03 <104310606+KingRajan03@users.noreply.github.com>
2024-01-08 20:35:35 +0530

    Create Count Good nodes in Binary tree.py

---
+++ Count Good nodes in Binary tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+        
+        # Return the count of good nodes obtained from the dfs function.
+        return dfs(root, root.val)
